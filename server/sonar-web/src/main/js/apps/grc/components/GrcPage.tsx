4486b186ab09a4853a9f5f309092867e9e9ead31
Jagadesh Paladugula <jagadesh.paladugula@autorabit.com>
2022-08-05 18:53:14 +0530

    Reloading the the page, when id is updated

--- server/sonar-web/src/main/js/apps/grc/components/GrcPage.tsx
+++ server/sonar-web/src/main/js/apps/grc/components/GrcPage.tsx
@@ -6,17 +6,21 @@ import {getComponentData, searchProjects} from "../../../api/components";
 import {WithRouterProps} from "react-router";
 import {Router, withRouter} from "../../../components/hoc/withRouter";
 import {ComponentContext} from "../../../app/components/ComponentContext";
+import { Location } from 'history'
+
 
 interface Props {
   children: React.ReactElement;
   setGrcUi: (grc: boolean) => void;
   router: Pick<Router, 'replace'>;
+  location:Location
 }
 
 function GrcPage({setGrcUi, children, router, location}: Props & WithRouterProps) {
 
   const [component, setComponent] = useState<T.Component>();
   const [loading, setLoading] = useState<boolean>();
+
 
   useEffect(() => {
     // Set 'grc' state variable to true when component is mounted.
@@ -29,20 +33,26 @@ function GrcPage({setGrcUi, children, router, location}: Props & WithRouterProps
     return () => {
       setGrcUi(false);
     };
-  }, []);
+  }, [location.query.id]);
 
   const loadGrcProjects = () => {
     setLoading(true);
     searchProjects({filter: 'tags=grc'})
     .then(({components}) => {
-      const {id} = location.query;
+      let {id} = location.query;
 
+      // if components are not there, if id is not given,
       if (!components.length) {
         return Promise.reject();
+      }else{
+        if(!id){
+          id = components[0].key;
+        }
       }
 
-      router.replace('/grc/dashboard?id='+components[0].key);
-      return getComponentData({component: id || components[0].key});
+
+      router.replace('/grc/dashboard?id='+id);
+      return getComponentData({component: id });
     })
     .then(({component}) => {
       setComponent(component);
@@ -51,7 +61,7 @@ function GrcPage({setGrcUi, children, router, location}: Props & WithRouterProps
       setLoading(false);
     });
   }
-
+  
   return (
       <div className="grc-container">
         {loading ? (
