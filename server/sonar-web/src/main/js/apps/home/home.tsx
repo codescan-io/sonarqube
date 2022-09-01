ef349db390e5f6ba34f59ba4ca13fa11d13fb21f
Abhinav Malladi <abhinav.malladi@autorabit.com>
2022-09-01 17:41:06 +0530

    changed default route.

--- server/sonar-web/src/main/js/apps/home/home.tsx
+++ server/sonar-web/src/main/js/apps/home/home.tsx
@@ -38,7 +38,7 @@ const home = () => {
                         </Link>
                     </div>
                     <div className="block">
-                        <Link to="/grc">
+                        <Link to="/grc/dashboard">
                             <img className="grc-icon" src='/images/grc/orgscan-dashboard.svg' alt="" />
                             <img className="grc-logo" src='/images/grc/orgscan-logo.svg' alt="" />
                             <p>Policy Management</p>
