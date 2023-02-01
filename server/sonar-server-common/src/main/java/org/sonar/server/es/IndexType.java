d84d7e4f53dd6ec3e09a8c5fdc58cc991439ade3
Jacek Poreda <jacek.poreda@sonarsource.com>
2023-02-01 17:03:06 +0100

    SONAR-17714 Drop types from ES requests

--- server/sonar-server-common/src/main/java/org/sonar/server/es/IndexType.java
+++ server/sonar-server-common/src/main/java/org/sonar/server/es/IndexType.java
@@ -165,13 +165,11 @@ public abstract class IndexType {
   public static final class IndexRelationType extends IndexType {
     private final IndexMainType mainType;
     private final String name;
-    private final String key;
 
     private IndexRelationType(IndexMainType mainType, String name) {
       this.mainType = mainType;
       checkArgument(name != null && !name.isEmpty(), "type name can't be null nor empty");
       this.name = name;
-      this.key = mainType.index.getName() + "/" + mainType.type + "/" + name;
     }
 
     @Override
@@ -185,7 +183,7 @@ public abstract class IndexType {
 
     @Override
     public String format() {
-      return key;
+      return mainType.index.getName()  + "/" + "_doc";
     }
 
     @Override
@@ -208,7 +206,7 @@ public abstract class IndexType {
 
     @Override
     public String toString() {
-      return "[" + key + "]";
+      return "[" + mainType.index.getName() + "/" + mainType.type + "/" + name + "]";
     }
   }
 }
