18222ca3e470344ea26aff27d93dc811420630dc
Sai Anurag Varanasi <saianurag.varanasi@autorabit.com>
2023-02-24 16:58:14 +0530

    version 23.0.3 update

--- server/sonar-web/src/main/js/app/components/embed-docs-modal/EmbedDocsPopup.tsx
+++ server/sonar-web/src/main/js/app/components/embed-docs-modal/EmbedDocsPopup.tsx
@@ -146,7 +146,7 @@ export class EmbedDocsPopup extends React.PureComponent<Props, State> {
           </a>
           {this.state.reseting && this.renderAboutCodescan(
             'https://knowledgebase.autorabit.com/codescan/docs/codescan-release-notes',
-            'embed-doc/codescan-version-23_0_2.png',
+            'embed-doc/codescan-version-23_0_3.png',
             translate('embed_docs.codescan_version')
           )}
         </li>
@@ -191,7 +191,7 @@ export class EmbedDocsPopup extends React.PureComponent<Props, State> {
           </a>
           {this.state.reseting && this.renderAboutCodescan(
             'https://knowledgebase.autorabit.com/codescan/docs/codescan-release-notes',
-            'embed-doc/codescan-version-23_0_2.png',
+            'embed-doc/codescan-version-23_0_3.png',
             translate('embed_docs.codescan_version')
           )}
         </li>
