b16c2cf75b8fc080e7ccc2a80ccf57263c62c373
Matteo Mara <matteo.mara@sonarsource.com>
2023-03-30 15:02:06 +0200

    SONAR-18745 Drop default field in api/qualitygates/list

--- server/sonar-webserver-webapi/src/main/java/org/sonar/server/qualitygate/ws/ListAction.java
+++ server/sonar-webserver-webapi/src/main/java/org/sonar/server/qualitygate/ws/ListAction.java
@@ -35,7 +35,6 @@ import org.sonar.server.qualitygate.QualityGateFinder;
 import org.sonarqube.ws.Qualitygates.ListWsResponse;
 import org.sonarqube.ws.Qualitygates.ListWsResponse.QualityGate;
 
-import static java.util.Optional.ofNullable;
 import static org.sonar.core.util.stream.MoreCollectors.toList;
 import static org.sonar.server.ws.WsUtils.writeProtobuf;
 
@@ -60,6 +59,7 @@ public class ListAction implements QualityGatesWsAction {
       .setSince("4.3")
       .setResponseExample(Resources.getResource(this.getClass(), "list-example.json"))
       .setChangelog(
+        new Change("10.0", "Field 'default' in the response has been removed"),
         new Change("10.0", "Field 'id' in the response has been removed"),
         new Change("9.9", "'caycStatus' field is added on quality gate"),
         new Change("8.4", "Field 'id' in the response is deprecated. Format changes from integer to string."),
@@ -94,7 +94,6 @@ public class ListAction implements QualityGatesWsAction {
           .setActions(wsSupport.getActions(dbSession, organization, qualityGate, defaultQualityGate))
           .build())
         .collect(toList()));
-    ofNullable(defaultUuid).ifPresent(builder::setDefault);
     return builder.build();
   }
 
