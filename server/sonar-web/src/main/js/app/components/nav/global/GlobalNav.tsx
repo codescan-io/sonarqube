c8d63fb02c8130a6a0da7f01b041ee5e2e8326d7
Barys Yakavita <barys.yakavita@autorabit.com>
2023-04-18 15:15:00 +0300

    CD-1989: Pendo integration

--- server/sonar-web/src/main/js/app/components/nav/global/GlobalNav.tsx
+++ server/sonar-web/src/main/js/app/components/nav/global/GlobalNav.tsx
@@ -34,11 +34,13 @@ import { Organization } from "../../../../types/types";
 export interface GlobalNavProps {
   currentUser: CurrentUser;
   userOrganizations: Organization[];
+  pendoInitialized: boolean
+  updatePendoInitialized: (pendoInitialized: boolean) => void;
   location: { pathname: string };
 }
 
 export function GlobalNav(props: GlobalNavProps) {
-  const { currentUser, userOrganizations, location } = props;
+  const { currentUser, userOrganizations, pendoInitialized, updatePendoInitialized, location } = props;
   return (
     <NavBar className="navbar-global" height={rawSizes.globalNavHeightRaw} id="global-navigation">
       <GlobalNavBranding />
@@ -49,7 +51,7 @@ export function GlobalNav(props: GlobalNavProps) {
         <EmbedDocsPopupHelper />
         <Search />
         {isLoggedIn(currentUser) && <GlobalNavPlus />}
-        <GlobalNavUser currentUser={currentUser} userOrganizations={userOrganizations}/>
+        <GlobalNavUser currentUser={currentUser} userOrganizations={userOrganizations} pendoInitialized={pendoInitialized} updatePendoInitialized={updatePendoInitialized}/>
       </div>
     </NavBar>
   );
