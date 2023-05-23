ce4c77acd43fb6ad9677df4240d37d615cb76f7b
Eric Giffon <eric.giffon@sonarsource.com>
2023-05-23 14:48:53 +0200

    SONAR-19340 Raise security hotspots events from CE

--- server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/issue/RuleRepositoryImpl.java
+++ server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/issue/RuleRepositoryImpl.java
@@ -225,5 +225,10 @@ public class RuleRepositoryImpl implements RuleRepository {
     public String getSeverity() {
       return addHocRule.getSeverity();
     }
+
+    @Override
+    public Set<String> getSecurityStandards() {
+      return Collections.emptySet();
+    }
   }
 }
