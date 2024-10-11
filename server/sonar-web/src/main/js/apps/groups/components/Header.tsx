SonarSource/sonarqube-webapp

SONAR-19346 Banner with GitHub sync status on Users, Groups, Authentication pages

server/sonar-web/src/main/js/apps/groups/components/Header.tsx
@@ -36,7 +36,7 @@ export default function Header(props: HeaderProps) {
 
   return (
     <>
-      <div className="page-header" id="groups-header">
+      <div className="page-header null-spacer-bottom" id="groups-header">
         <h2 className="page-title">{translate('user_groups.page')}</h2>
 
         <div className="page-actions">

codescan-io/sonarqube

server/sonar-web/src/main/js/apps/groups/components/Header.tsx
@@ -59,7 +59,7 @@ export default class Header extends React.PureComponent<Props, State> {
   render() {
     return (
       <>
-        <header className="page-header" id="groups-header">
+        <header className="page-header null-spacer-bottom" id="groups-header">
           <h1 className="page-title">{translate('user_groups.page')}</h1>
 
           <div className="page-actions">