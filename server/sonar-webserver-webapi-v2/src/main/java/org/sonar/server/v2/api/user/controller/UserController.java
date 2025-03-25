3162ff7f667acd340619de76fbec9c64007e97c4
saikiran-metla <156284791+saikiran-metla@users.noreply.github.com>
2025-03-25 15:17:55 +0530

    Removed fetch single user api by commenting out as it is not being used anywhere

--- server/sonar-webserver-webapi-v2/src/main/java/org/sonar/server/v2/api/user/controller/UserController.java
+++ server/sonar-webserver-webapi-v2/src/main/java/org/sonar/server/v2/api/user/controller/UserController.java
@@ -82,21 +82,21 @@ public interface UserController {
     @PathVariable("id") @Parameter(description = "The ID of the user to delete.", required = true, in = ParameterIn.PATH) String id,
     @RequestParam(value = "anonymize", required = false, defaultValue = "false") @Parameter(description = "Anonymize user in addition to deactivating it.") Boolean anonymize);
 
-  @GetMapping(path = "/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  @Operation(summary = "Fetch a single user", description = """
-    Fetch a single user.
-    The following fields are only returned when user has Administer System permission or for logged-in in user :
-        'email'
-        'externalIdentity'
-        'externalProvider'
-        'groups'
-        'lastConnectionDate'
-        'sonarLintLastConnectionDate'
-        'tokensCount'
-      Field 'sonarqubeLastConnectionDate' is only updated every hour, so it may not be accurate, for instance when a user authenticates many times in less than one hour.
-    """)
-  UserRestResponse fetchUser(@PathVariable("id") @Parameter(description = "The id of the user to fetch.", required = true, in = ParameterIn.PATH) String id);
+//  @GetMapping(path = "/{id}")
+//  @ResponseStatus(HttpStatus.OK)
+//  @Operation(summary = "Fetch a single user", description = """
+//    Fetch a single user.
+//    The following fields are only returned when user has Administer System permission or for logged-in in user :
+//        'email'
+//        'externalIdentity'
+//        'externalProvider'
+//        'groups'
+//        'lastConnectionDate'
+//        'sonarLintLastConnectionDate'
+//        'tokensCount'
+//      Field 'sonarqubeLastConnectionDate' is only updated every hour, so it may not be accurate, for instance when a user authenticates many times in less than one hour.
+//    """)
+//  UserRestResponse fetchUser(@PathVariable("id") @Parameter(description = "The id of the user to fetch.", required = true, in = ParameterIn.PATH) String id);
 
   @PatchMapping(path = "/{id}", consumes = JSON_MERGE_PATCH_CONTENT_TYPE, produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.OK)
