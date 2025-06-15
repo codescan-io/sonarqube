c210421e2232fe93e826ae1a61e31bdc81e457e3
Dhanush Terala <dhanush.terala@autorabit.com>
2025-06-15 17:42:02 +0530

    CD-6676 added error handling with oauth

--- server/sonar-web/src/main/js/apps/account/organizations/UserOrganizations.tsx
+++ server/sonar-web/src/main/js/apps/account/organizations/UserOrganizations.tsx
@@ -17,18 +17,48 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import * as React from 'react';
+
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { ButtonPrimary } from '~design-system';
+import withAppStateContext from '../../../app/components/app-state/withAppStateContext';
+import withCurrentUserContext from '../../../app/components/current-user/withCurrentUserContext';
+import { translate } from '../../../helpers/l10n';
+import { AppState } from '../../../types/appstate';
+import { GlobalSettingKeys } from '../../../types/settings';
+import { Organization } from '../../../types/types';
+import '../projects/account.css';
 import OrganizationsList from './OrganizationsList';
-import { Organization } from "../../../types/types";
-import { translate } from "../../../helpers/l10n";
-import Link from "../../../components/common/Link";
-import { AppState } from "../../../types/appstate";
-import withAppStateContext from "../../../app/components/app-state/withAppStateContext";
-import withCurrentUserContext from "../../../app/components/current-user/withCurrentUserContext";
-import { GlobalSettingKeys } from "../../../types/settings";
-import "../projects/account.css";
-import { ButtonPrimary } from "~design-system";
+
+interface AlmConnection {
+  clientId: string;
+  clientSecret: string;
+  organizationKee: string;
+  url: string;
+  uuid: string;
+}
+
+function decrypt(value: string): string {
+  return value;
+}
+
+const getAlmConnectionFromLocalStorage = (): AlmConnection | null => {
+  const stored = localStorage.getItem('almConnectionTemp');
+  if (!stored) {
+    return null;
+  }
+
+  const encryptedAlmConnection = JSON.parse(stored);
+
+  const decryptedAlmConnection: AlmConnection = {
+    ...encryptedAlmConnection,
+    url: decrypt(encryptedAlmConnection.url),
+    clientId: decrypt(encryptedAlmConnection.clientId),
+    clientSecret: decrypt(encryptedAlmConnection.clientSecret),
+  };
+
+  return decryptedAlmConnection;
+};
 
 interface Props {
   appState: AppState;
@@ -36,30 +66,50 @@ interface Props {
 }
 
 function UserOrganizations(props: Props) {
-
-  const { appState: { settings, canAdmin, canCustomerAdmin }, userOrganizations } = props;
+  const {
+    appState: { settings, canAdmin, canCustomerAdmin },
+    userOrganizations,
+  } = props;
   const anyoneCanCreate = settings[GlobalSettingKeys.OrganizationsAnyoneCanCreate] === 'true';
-  const canCreateOrganizations = (anyoneCanCreate || canAdmin || canCustomerAdmin);
+  const canCreateOrganizations = anyoneCanCreate || canAdmin || canCustomerAdmin;
+
+  useEffect(() => {
+    const rawHash = window.location.hash.startsWith('#') ? window.location.hash.substring(1) : '';
+    const params = new URLSearchParams(rawHash);
+    if (params.has('error')) {
+      const existingConfig = getAlmConnectionFromLocalStorage();
+      if (existingConfig) {
+        let redirectUrl = `/organizations/${existingConfig.organizationKee}/extension/developer/alm#almId=${encodeURIComponent(existingConfig.uuid || '')}`;
+        if (params.has('error')) {
+          redirectUrl += `&error=${encodeURIComponent(params.get('error') || '')}`;
+        }
+        if (params.has('error_description')) {
+          redirectUrl += `&error_description=${encodeURIComponent(params.get('error_description') || '')}`;
+        }
+        window.location.href = redirectUrl;
+      }
+    }
+  }, []);
 
   return (
-      <div className="account-body account-container organization-card-ctnr">
-        <Helmet title={translate('my_account.organizations')}/>
+    <div className="account-body account-container organization-card-ctnr">
+      <Helmet title={translate('my_account.organizations')} />
 
-        <div className="boxed-group">
-          {canCreateOrganizations && (
-              <div className="clearfix">
-                <div className="boxed-group-actions sw-flex sw-justify-end sw-mb-4">
-                  <ButtonPrimary className="button " to="/organizations/create">
-                    {translate('create')}
-                  </ButtonPrimary>
-                </div>
-              </div>
-          )}
-          <div className="boxed-group-inner">
-            <OrganizationsList organizations={userOrganizations}/>
+      <div className="boxed-group">
+        {canCreateOrganizations && (
+          <div className="clearfix">
+            <div className="boxed-group-actions sw-flex sw-justify-end sw-mb-4">
+              <ButtonPrimary className="button " to="/organizations/create">
+                {translate('create')}
+              </ButtonPrimary>
+            </div>
           </div>
+        )}
+        <div className="boxed-group-inner">
+          <OrganizationsList organizations={userOrganizations} />
         </div>
       </div>
+    </div>
   );
 }
 
