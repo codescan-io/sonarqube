0e2f1b2b1a7d0e2c2f1e45eb968660019ede7831
Rajashekar-codescan <rajashekar.chekka@autorabit.com>
2025-11-13 16:30:24 +0530

    CD-7076 Added session storage clear when login

--- server/sonar-web/src/main/js/apps/sessions/components/Login.tsx
+++ server/sonar-web/src/main/js/apps/sessions/components/Login.tsx
@@ -51,6 +51,7 @@ export interface LoginProps {
 }
 
 export default function Login(props: Readonly<LoginProps>) {
+   clearChatSession();
   const { identityProviders, loading, location, message, accessConsentMessage } = props;
   const returnTo = getReturnUrl(location);
   const displayError = Boolean(location.query.authorizationError);
@@ -94,6 +95,13 @@ export default function Login(props: Readonly<LoginProps>) {
       </Card>
     </div>
   );
+}
+
+export function clearChatSession() {
+   console.log("Chat messages cleared from sessionStorage on logout.");
+  if (typeof window !== 'undefined') {
+    sessionStorage.removeItem('chatMessages');  // clears chat only
+  }
 }
 
 const StyledMessage = styled.div`
