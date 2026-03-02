2fe11a231d1031cfcbc2ab482cd98a6db7d564a2
Dhanush Terala <dhanush.terala@autorabit.com>
2026-03-03 01:24:07 +0530

    CD-7905 refactored code which is Implemented as part of CD-6276.

--- server/sonar-db-dao/src/main/java/org/sonar/db/issue/IssueMapper.java
+++ server/sonar-db-dao/src/main/java/org/sonar/db/issue/IssueMapper.java
@@ -88,8 +88,4 @@ public interface IssueMapper {
   void deleteIssueImpacts(String issueKey);
 
   void updateHotspotExceptionExpiryDate(@Param("issueKey") String issueKey, @Param("expiryDate") Long expiryDate);
-
-  void bulkClearHotspotExceptionExpiryDate(@Param("keys") List<String> keys);
-
-  List<String> selectExpiredHotspotKeys(@Param("currentTime") long currentTime);
 }
