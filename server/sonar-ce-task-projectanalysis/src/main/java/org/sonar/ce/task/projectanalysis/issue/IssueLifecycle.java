af18d8a5df6432325c2024e890cd0344d6cd35a2
mohith-shaik <mohith.shaik@autorabit.com>
2026-07-13 15:40:59 +0530

    [CD-9059-NEW] | PR changes

--- server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/issue/IssueLifecycle.java
+++ server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/issue/IssueLifecycle.java
@@ -22,6 +22,7 @@ package org.sonar.ce.task.projectanalysis.issue;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import java.util.Date;
+import java.util.Objects;
 import java.util.Optional;
 import javax.inject.Inject;
 import org.jetbrains.annotations.NotNull;
@@ -211,7 +212,7 @@ public class IssueLifecycle {
     copyFields(raw, base);
     base.changes().forEach(raw::addChange);
 
-    if (raw.isAiFixSupported() != base.isAiFixSupported()) {
+    if (!Objects.equals(raw.getVariableType(), base.getVariableType())) {
       raw.setChanged(true);
     }
 
